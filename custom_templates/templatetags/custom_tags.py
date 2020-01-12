24e41daf46ce635b94eeae4d4a75b536e25fff93
Theo Lipeles <Theo@Lipeles.com>
2020-01-12 16:30:59 -0500

    adds matching box skeleton

--- custom_templates/templatetags/custom_tags.py
+++ custom_templates/templatetags/custom_tags.py
@@ -5,3 +5,7 @@ register = template.Library()
 @register.inclusion_tag('_delayed_next.html')
 def delayed_next(wait=2000):
     return {}
+
+@register.inclusion_tag('_matching_box.html')
+def matching_box():
+    return {}
