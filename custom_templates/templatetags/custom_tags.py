358196bbb60c01ec4a206f5200f01213fc65e49a
Theo Lipeles <Theo@Lipeles.com>
2020-01-16 04:13:16 -0500

    adds customization to delayed_next tag

--- custom_templates/templatetags/custom_tags.py
+++ custom_templates/templatetags/custom_tags.py
@@ -6,15 +6,17 @@ with open(os.path.join(os.getcwd(), 'boxes.json')) as json_file:
     boxes = json.load(json_file)
 
 @register.inclusion_tag('_delayed_next.html')
-def delayed_next(wait=2000):
-    return {}
+def delayed_next(wait=2000, label="NEXT"):
+    return {
+        "wait": wait,
+        "label": label
+    }
 
 @register.inclusion_tag('_matching_box.html')
 def matching_box(choices=["...","...","...","...",0,"...","...","...","..."], matching_box_id="matching_box", rounds=1):
-    template_vars = {
+    return {
         "choices": choices,
         "matching_box_id": matching_box_id,
         "rounds": rounds,
         "boxes": boxes
     }
-    return template_vars
