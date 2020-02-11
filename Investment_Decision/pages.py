7869551241fec2a0d4cce45de69436d6c6db0b99
Theo Lipeles <Theo@Lipeles.com>
2020-02-11 16:54:38 -0500

    adds Investment Decision page

--- Investment_Decision/pages.py
+++ Investment_Decision/pages.py
@@ -1,19 +1,21 @@
 from otree.api import Currency as c, currency_range
 from ._builtin import Page, WaitPage
 from .models import Constants
+import time
 
 
-class MyPage(Page):
+class Instructions(Page):
+    form_model = 'player'
+    form_fields = ['Points_A', 'Points_B']
+    def error_message(self, values):
+        print('values is', values)
+        if values['Points_A'] + values['Points_B']  != 100:
+            return 'The numbers must add up to 100'
+    def before_next_page(self):
+        # user has 5 minutes to complete as many pages as possible
+        self.participant.vars['expiry'] = time.time() + 90
     pass
 
 
-class ResultsWaitPage(WaitPage):
-    def after_all_players_arrive(self):
-        pass
 
-
-class Results(Page):
-    pass
-
-
-page_sequence = [MyPage, ResultsWaitPage, Results]
+page_sequence = [Instructions]
