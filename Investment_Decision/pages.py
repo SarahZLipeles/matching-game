c7228c5023c29c2e7d7d549812bcdb4b3dec74b3
Theo Lipeles <Theo@Lipeles.com>
2020-02-11 16:56:45 -0500

    removes error message

--- Investment_Decision/pages.py
+++ Investment_Decision/pages.py
@@ -8,9 +8,8 @@ class Instructions(Page):
     form_model = 'player'
     form_fields = ['Points_A', 'Points_B']
     def error_message(self, values):
-        print('values is', values)
         if values['Points_A'] + values['Points_B']  != 100:
-            return 'The numbers must add up to 100'
+            return 'The points must add up to 100'
     def before_next_page(self):
         # user has 5 minutes to complete as many pages as possible
         self.participant.vars['expiry'] = time.time() + 90
